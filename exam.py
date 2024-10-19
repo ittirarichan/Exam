8aeb46fbc3594279a5dfb46be6c7b96f951a09ad
ittirarichan <abhishekbinish86@gmail.com>
2024-10-19 11:27:10 +0530

    exam

---
+++ exam.py
@@ -0,0 +1,47 @@
+# a=65
+# for i in range(3):
+#     for j in range(i+1):
+#         print(chr(a-j),end="\t")
+#     print()
+#     a+=1
+
+
+# n = 3
+# for i in range(n):
+#     if i % 2 == 0:
+#         for j in range(n):
+#             print(chr(65 + j), end=" ")
+#     else:
+#         for j in range(n - 1, -1, -1):
+#             print(chr(65 + j), end="")
+#     print()
+
+
+# n = 65 
+# for i in range(3):
+#     a=1
+#     for j in range(3):
+#         if i == j:
+#             print(a, end=" ")
+#         else:
+#             print(chr(n), end=" ")
+#     print()
+#     n+=1
+
+
+
+# ----------------------------------------------------
+# a=65
+# for i in range(3, 0, -1):
+#     for j in range(i):
+#         print(chr(a+j), end="\t")
+#     print()
+
+    
+for i in range(1,):
+    for j in range(i):
+        print((i+j),end='\t')
+
+
+
+
