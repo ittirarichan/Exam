48808794dfaadd17c980ec7d352be646fbeb8cf1
ittirarichan <abhishekbinish86@gmail.com>
2024-11-02 11:17:38 +0530

    exam nov 2

---
+++ nov2.py
@@ -0,0 +1,20 @@
+# l=['hai','malayalam','welcome']
+# for i in l:
+#     a=len(i)
+#     if a>i:
+#         print(i)
+#     else:
+#         print(i)
+
+
+
+# ----------------------------------------------
+
+# correct
+
+
+l=['hai','malayalam','welcome']
+large=''
+for i in l:
+    len(i)>len(large)
+print(i)
